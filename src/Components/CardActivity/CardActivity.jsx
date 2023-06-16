8e0a33e30852ba9362fd58430ef1983c7df3e3bd
averous <pierreaverous42@gmail.com>
2023-06-16 10:14:41 +0200

    ajout

--- src/Components/CardActivity/CardActivity.jsx
+++ src/Components/CardActivity/CardActivity.jsx
@@ -1,26 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CardActivityStyles.scss";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
+import VisibilitySensor from "react-visibility-sensor";
 
-const CardActvity = (props) => {
+const CardActivity = (props) => {
+    const [isVisible, setIsVisible] = useState(false);
 
-    return(
-        <>
-            <div className='containerCard'>
-              <div className='card'>
-                  <Link to={{
-                      pathname: `/activity/${props.dataimg.id}`,
-                      state: {id: props.dataimg.id}
-                  }}>
-                    <img className='CardBackground'src={props.dataimg.img} alt={props.dataimg.title}/>
-                  </Link>
-                  <p className='textCard'>{props.dataimg.title}</p>
-              </div>
+    const handleVisibilityChange = (visible) => {
+        if (visible) {
+            setIsVisible(true);
+        }
+    };
+
+    return (
+        <VisibilitySensor onChange={handleVisibilityChange}>
+            <div className={`containerCard ${isVisible ? "visible" : ""}`}>
+                <div className={`card ${isVisible ? "visible" : ""}`}>
+                    <Link
+                        to={{
+                            pathname: `/activity/${props.dataimg.id}`,
+                            state: { id: props.dataimg.id },
+                        }}
+                    >
+                        <img
+                            className={`CardBackground ${isVisible ? "visible" : ""}`}
+                            src={props.dataimg.img}
+                            alt={props.dataimg.title}
+                        />
+                    </Link>
+                    <p className={`textCard ${isVisible ? "visible" : ""}`}>
+                        {props.dataimg.title}
+                    </p>
+                </div>
             </div>
-        </>
+        </VisibilitySensor>
+    );
+};
 
-    )
-
-}
-
-export default CardActvity
+export default CardActivity;
--- src/Pages/PageDetailsActivité/ActivityDetailPage.jsx
+++ src/Pages/PageDetailsActivité/ActivityDetailPage.jsx
@@ -1,8 +1,7 @@
-
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import "./ActivityDetailPagesStyles.scss"
 import fetchById from "../../Data/api";
-import {useParams} from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 import HeaderNavBar from "../../Components/HeaderNavBar/HeaderNavBar";
 import Footer from "../../Components/Footer/Footer";
 import CarousselBanner from "../../Components/Caroussel/CarrousselBanner";
@@ -10,46 +9,52 @@ import TitleActivity from "../../Components/TitleActivity/TitleActivity";
 import DescritpionActivity from "../../Components/DescritpionActivity/DescritpionActivity";
 import VideoActivity from "../../Components/VideoActivity/VideoActivity";
 
-
 const ActivityDetailPage = () => {
-    const [dataActivityId, setActivityId] = useState(null)
+    const [dataActivityId, setActivityId] = useState(null);
     const result = useParams();
+    const location = useLocation();
+
     const loadActivityId = async () => {
-        // console.log(fetchById())
-        const dataActivity = await fetchById(result.id)
-        setActivityId(dataActivity)
+        const dataActivity = await fetchById(result.id);
+        setActivityId(dataActivity);
+    };
 
-    }
-    useEffect(()=>{
-        loadActivityId()
+    useEffect(() => {
+        loadActivityId();
+        window.scrollTo(0, 0); // Faire défiler vers le haut lorsque le composant se monte
+    }, [result.id]);
 
-    },[result.id])
+    useEffect(() => {
+        const scrollElement = document.getElementById("activityPage");
+        if (scrollElement && location.hash) {
+            scrollElement.scrollIntoView({
+                behavior: "smooth",
+                block: "start",
+            });
+        }
+    }, [location.hash]);
 
-
-    return(
+    return (
         <>
             {dataActivityId != null && (
                 <>
-                    <HeaderNavBar/>
-                    <TitleActivity dataActivityId={dataActivityId}/>
-                    <CarousselBanner dataActivityId={dataActivityId}/>
-                    <DescritpionActivity dataActivityId={dataActivityId}/>
-                    <div className='containerActivityVideo'>
-                        <h2 className='titleVideo'>Découvrez nos video de l'activité : </h2>
-                        <div className='container-video'>
-                            {dataActivityId.video.map(d => <VideoActivity dataActivityId={d}/>)}
+                    <HeaderNavBar />
+                    <div id="activityPage">
+                        <TitleActivity dataActivityId={dataActivityId} />
+                        <CarousselBanner dataActivityId={dataActivityId} />
+                        <DescritpionActivity dataActivityId={dataActivityId} />
+                        <div className='containerActivityVideo'>
+                            <h2 className='titleVideo'>Découvrez nos vidéos de l'activité :</h2>
+                            <div className='container-video'>
+                                {dataActivityId.video.map(d => <VideoActivity dataActivityId={d} />)}
+                            </div>
                         </div>
                     </div>
-
-                    <Footer/>
+                    <Footer />
                 </>
             )}
-
-
-
         </>
+    );
+};
 
-    )
-}
-
-export default ActivityDetailPage
+export default ActivityDetailPage;
